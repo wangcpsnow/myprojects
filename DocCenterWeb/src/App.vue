0c4967edd12ae4dc26681800951c0c0f5c987805
王春鹏 <wangcpsnow@gmail.com>
2017-11-27 21:41:20 +0800

    add sidebar

--- DocCenterWeb/src/App.vue
+++ DocCenterWeb/src/App.vue
@@ -1,23 +1,51 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+    	<div class="nav">
+			nav
+		</div>
+		<div class="wrap">
+			<sidebar></sidebar>
+			<div class="main">
+				<router-view/>
+			</div>
+		</div>
+    </div>
 </template>
 
 <script>
+import sidebar from './components/sidebar'
+
 export default {
-  name: 'app'
+	name: 'app',
+	components: {
+		sidebar
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+}
+.nav {
+	height: 110px;
+	background-color: #0b0b0b;
+}
+.wrap {
+	overflow: hidden;
+	zoom: 1;
+}
+.sidebar, .main {
+	margin-bottom: -999px;
+	padding-bottom: 999px;
+}
+.sidebar{
+	float: left;
+	width: 220px;
+}
+.main {
+	margin-left: 220px;
 }
 </style>
---
+++ DocCenterWeb/src/components/sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="sidebar">
+		<ul>
+			<li>
+				综合维修信息
+			</li>
+			<li>
+				技术信息
+			</li>
+		</ul>	
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style>
+.sidebar {
+	background-color: #3e4349;
+	color: #FFF;
+}
+</style>
