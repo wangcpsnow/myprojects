0fd1599a33421679cfd795663410c2b99f317685
王春鹏 <wangcpsnow@gmail.com>
2017-11-30 22:20:48 +0800

    update sidebar

--- DocCenterWeb/src/components/sidebar.vue
+++ DocCenterWeb/src/components/sidebar.vue
@@ -2,12 +2,36 @@
 	<div class="sidebar">
 		<ul>
 			<li>
-				综合维修信息
+				<a href="javascirpt:void(0)">
+					综合维修信息<s class="sleft"></s><s></s>
+				</a>
 			</li>
 			<li>
-				技术信息
+				<a href="javascirpt:void(0)">
+					技术信息<s class="sleft"></s><s></s>
+				</a>
 			</li>
-		</ul>	
+			<li>
+				<a href="javascirpt:void(0)">
+					主动学习与支持<s class="sleft"></s><s></s>
+				</a>
+			</li>
+			<li>
+				<a href="javascirpt:void(0)">
+					综合维修信息<s class="sleft"></s><s></s>
+				</a>
+			</li>
+			<li>
+				<a href="javascirpt:void(0)">
+					技术信息<s class="sleft"></s><s></s>
+				</a>
+			</li>
+			<li>
+				<a href="javascirpt:void(0)">
+					主动学习与支持<s class="sleft"></s><s></s>
+				</a>
+			</li>
+		</ul>
 	</div>
 </template>
 
@@ -19,5 +43,31 @@
 .sidebar {
 	background-color: #3e4349;
 	color: #FFF;
+	padding-top: 40px;
+	padding-right: 30px;
+}
+.sidebar li {
+	line-height: 47px;
+	margin-bottom: 15px;
+	border: 1px solid #707987;
+	border-left: 0 none;
+	background-color: #727b89;
+	padding-left: 14px;
+}
+.sidebar li a {
+	color: #FFF;
+	display: block;
+}
+.sidebar li s {
+	border: 8px solid transparent;
+	border-left-color: #FFF;
+	float: right;
+	margin-top: 13px;
+	
+}
+.sidebar li s.sleft {
+	border-left-color: #727b89;
+	position: relative;
+	left: -18px;
 }
 </style>
