0703a5b6466ec9d5eec063db4fb8e0612909ec9a
wangchunpeng <wangchunpeng@baidu.com>
2018-05-23 19:48:33 +0800

    add BillCMS

---
+++ BillCMS/src/components/Login.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="login">
+        <h3 class="center title">发票星后台摇奖登录系统</h3>
+        <el-form ref="form" :model="loginForm" label-width="80px">
+            <el-form-item label="账号">
+                <el-input v-model="loginForm.userName"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="loginForm.pwd" type='password'></el-input>
+            </el-form-item>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="click_login">登录</el-button>
+            </el-form-item>
+            <p class="center tips">联系我们：15210831061</p>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                loginForm: {
+                    userName: '',
+                    pwd: ''
+                }
+            }
+        },
+        created() {
+
+        },
+        methods: {
+            click_login() {
+                if (!this.loginForm.userName || !this.loginForm.pwd) {
+                    this.$toast('请输入账号密码');
+                    return;
+                }
+                this.$http.post('/ild/admin/manage/login', this.loginForm)
+                    .then(res => {
+
+                    });
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .login {
+        width: 400px;
+        margin: auto;
+        margin-top: 100px;
+        top: 50%
+        .title {
+            margin-bottom: 30px;
+        }
+        .tips {
+            margin-top: 100px;
+        }
+    }
+</style>
---
+++ BillCMS/src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import axios from 'axios'
+
+import './assets/reset.css'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.config.productionTip = false;
+
+Vue.prototype.$toast = function (title, txt='', type='success') {
+    Vue.prototype.$notify({
+        title: title,
+        message: txt,
+        type: type
+    });
+}
+Vue.prototype.$http = axios;
+Vue.use(ElementUI);
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
