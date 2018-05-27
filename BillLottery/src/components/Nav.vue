9ed056bf5bb105e92917f22e364c8d3972fc0aa8
wangchunpeng <wangchunpeng@baidu.com>
2018-05-27 09:11:41 +0800

    BillLottery add live url

--- BillLottery/src/components/Nav.vue
+++ BillLottery/src/components/Nav.vue
@@ -4,8 +4,8 @@
             <tr>
                 <td :class='{active: activeType=="enter"}'><a href="/entry.html">发票录入</a></td>
                 <td :class='{active: activeType=="query"}'><a href="/query.html">中奖查询</a></td>
-                <td><a href="javascript:void(0)">直播与回顾</a></td>
-                <td><a href="javascript:void(0)">公告信息</a></td>
+                <td><a href="https://vzan.com/live/livedetail-233679?stamp=636583476286167584">直播与回顾</a></td>
+                <td><a href="http://weixin.121mai0078.com/index.php?g=Wap&m=Infomation&a=recommend_content&id=240859&token=uxgqhr1483508444">公告信息</a></td>
             </tr>
         </table>
     </div>
